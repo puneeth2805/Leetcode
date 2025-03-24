8cdcfacc7954a22d3e037d2a629b13195f4e9571
puneeth2805 <vorugantipuneeth2805@gmail.com>
2025-03-24 15:55:21 +0530

    Time: 175 ms (59.95%), Space: 52.8 MB (55.42%) - LeetHub

---
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        meetings.sort()
+        free_days = 0
+        latest_end = 0
+        for start, end in meetings:
+            if start > latest_end + 1:
+                free_days += start - latest_end - 1
+            latest_end = max(latest_end, end)
+        free_days += days - latest_end
+
+        return free_days
+        
+
+        
+
+
+        
